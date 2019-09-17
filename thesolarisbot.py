1408039b713607185c5a89166c44945a0d88c5c8
Alena <alenasal@hotmail.com>
2019-09-18 01:58:55 +0800

    vending basic functions

--- thesolarisbot.py
+++ thesolarisbot.py
@@ -33,7 +33,23 @@ else:
 def start_handler(bot, update):
     # Creating a handler-function for /start command
     logger.info("User {} started bot".format(update.effective_user["id"]))
-    update.message.reply_text("Hello {}!\nPress /random to get random number".format(update.effective_user["id"]))
+    update.message.reply_text("Hello {}!\nPress /vendingnow to activate the secret vending machine alert or /vending20 to inform your pals 20 mins in advance.\nPress /vendingfaulty to deliver the unfortunate news of the vending machine breakdown.\nPress /vendingworking to inform everyone of the revival of the vending machine.".format(update.effective_user["first_name"]))
+
+def vending_handler(bot, update):
+    logger.info("User {} activated the vending machine alert".format(update.effective_user["first_name"]))
+    update.message.reply_text("{} hereby activates the secret chat. We can look forward to a fully stocked vending machine now.".format(update.effective_user["first_name"]))
+
+def vending20_handler(bot, update):
+    logger.info("User {} activated the vending machine alert 20 mins in advance".format(update.effective_user["first_name"]))
+    update.message.reply_text("{} hereby activates the secret chat. In approximately 20 mins, we can look forward to a fully stocked vending machine.".format(update.effective_user["first_name"]))
+
+def vendingfaulty_handler(bot, update):
+    logger.info("User {} activated the faulty vending machine alert".format(update.effective_user["first_name"]))
+    update.message.reply_text("{} regrets to inform everyone that the vending machine is faulty. :(".format(update.effective_user["first_name"]))
+
+def vendingworking_handler(bot, update):
+    logger.info("User {} activated the working vending machine alert".format(update.effective_user["first_name"]))
+    update.message.reply_text("The vending machine has been fixed! Rejoice!!")
 
 
 def random_handler(bot, update):
@@ -43,11 +59,21 @@ def random_handler(bot, update):
     update.message.reply_text("Random number: {}".format(number))
 
 
+
+
+
 if __name__ == '__main__':
     logger.info("Starting bot")
     updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
     updater.dispatcher.add_handler(CommandHandler("random", random_handler))
+    updater.dispatcher.add_handler(CommandHandler("vendingnow", vending_handler))
+    updater.dispatcher.add_handler(CommandHandler("vending20", vending20_handler))
+    updater.dispatcher.add_handler(CommandHandler("vendingfaulty", vendingfaulty_handler))
+    updater.dispatcher.add_handler(CommandHandler("vendingworking", vendingworking_handler))
+
+
+
 
     run(updater)
