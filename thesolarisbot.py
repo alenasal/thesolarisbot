96bbde68975cd3d0da0805cd4160745a2c8dda52
Alena <alenasal@hotmail.com>
2019-09-26 00:05:28 +0800

    added jios

--- thesolarisbot.py
+++ thesolarisbot.py
@@ -3,7 +3,10 @@ import os
 import random
 import sys
 from datetime import date
-
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import CallbackQuery
+from telegram.ext import CallbackQueryHandler
+from telegram import ParseMode
 
 from telegram.ext import Updater, CommandHandler
 
@@ -85,10 +88,98 @@ def love_handler(update, context):
 
 def random_handler(update, context):
     user = update.message.from_user
-    # Creating a handler-function for /random command
     number = random.randint(0, 9999)
     logger.info("User %s random number ", user.first_name)
     update.message.reply_text("Here's a random 4-digit number: " + str(number))
+
+
+####### JIOS #########
+
+
+
+HELP_BUTTON_CALLBACK_DATA = 'Jio joined'
+LEAVE_BUTTON_CALLBACK_DATA = 'Jio left'
+
+
+help_button = [InlineKeyboardButton(
+    text='Join', # text that is shown to user
+    callback_data=HELP_BUTTON_CALLBACK_DATA # text that is sent to bot when user tap button
+    ),InlineKeyboardButton(
+    text='Leave', # text that is shown to user
+    callback_data=LEAVE_BUTTON_CALLBACK_DATA # text that is sent to bot when user tap button
+    ),]
+
+def command_handler_start(update,context):
+    global state_of_jios
+    if state_of_jios == True:
+        update.message.reply_text("Please close the previous jio with /closejio before opening a new jio.")
+    else:
+        user = update.message.from_user
+        if len(context.args) <= 1:
+            update.message.reply_text("Please add a title(without spaces) and time after /jio. \n(Eg. /jio pingpong 1830)")
+        else:
+            global jio_text
+            state_of_jios = True
+            jio_text = user.first_name + " has jioed for " + context.args[0] + " at " + context.args[1] + ". Click 'join' if you are interested!\n\nMinions attending:"
+            list_of_attendees.append(user.first_name)
+            markdown_text = markdown_output()
+            update.message.reply_text(
+                text=jio_text + markdown_text,
+                reply_markup=InlineKeyboardMarkup([help_button]),
+                )
+
+def command_handler_help(update,context):
+    user_name = update.effective_user.first_name
+    try:
+        if user_name not in list_of_attendees:
+            list_of_attendees.append(user_name)
+
+        markdown_text = markdown_output()
+        update.callback_query.edit_message_text(text=jio_text + markdown_text,
+                                                reply_markup=InlineKeyboardMarkup([help_button]), )
+    except:
+        logger.info("pressed button again")
+
+
+def command_handler_leave(update,context):
+    user_name = update.effective_user.first_name
+    try:
+        if user_name in list_of_attendees:
+            list_of_attendees.remove(user_name)
+
+        markdown_text = markdown_output()
+        update.callback_query.edit_message_text(text=jio_text + markdown_text,
+                                                reply_markup=InlineKeyboardMarkup([help_button]), )
+    except:
+        logger.info("pressed button again")
+
+
+def callback_query_handler(update,context):
+    cqd = update.callback_query.data
+    #jio_text = update.callback_query.message.text
+    if cqd == HELP_BUTTON_CALLBACK_DATA:
+        command_handler_help(update,context)
+    elif cqd == LEAVE_BUTTON_CALLBACK_DATA:
+        command_handler_leave(update,context)
+
+
+def markdown_output():
+    string ="\n"
+    string += "".join(["- " + str(s) + "\n" for s in list_of_attendees])
+    return string
+
+
+def closejio_handler(update, context):
+    user = update.message.from_user
+    logger.info("User %s has closed the jio", user.first_name)
+    markdown_text = markdown_output()
+    update.message.reply_text("Jio closed. You may now start a new jio.")
+    global state_of_jios
+    state_of_jios = False
+    global list_of_attendees
+    list_of_attendees= []
+
+##########################################################
 
 
 if __name__ == '__main__':
@@ -96,6 +187,12 @@ if __name__ == '__main__':
     updater = Updater(TOKEN, use_context=True)
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
+
+    global list_of_attendees
+    list_of_attendees = []
+
+    global state_of_jios
+    state_of_jios = False
 
     dp.add_handler(CommandHandler("start", start_handler))
     dp.add_handler(CommandHandler("vending", vending_handler))
@@ -107,6 +204,15 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler("encouragement", encouragement_handler))
     dp.add_handler(CommandHandler("love", love_handler))
     dp.add_handler(CommandHandler("random", random_handler))
+    dp.add_handler(CommandHandler("jio", command_handler_start))
+    dp.add_handler(CommandHandler('jiohelper', command_handler_help))
+    dp.add_handler(CommandHandler('jiohelperleave', command_handler_leave))
+    dp.add_handler(CommandHandler("closejio", closejio_handler))
+
+
+
+    dp.add_handler(CallbackQueryHandler(callback_query_handler))
+    updater.start_polling()
 
 
 
